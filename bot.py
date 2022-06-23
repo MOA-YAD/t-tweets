f8f337d8d7c19e6567764cc37758a0edaf56fa11
MOAYAD <101659441+MOA-YAD@users.noreply.github.com>
2022-06-23 10:09:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,32 +21,13 @@ xavierbot = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token
 moment_worker = []
 
 
-#start
-@xavierbot.on(events.NewMessage(pattern="^/start$"))
-async def start(event):
-  await event.reply("مـرحبآ بكـ في بوت التاك 😊❤️.!\n استطيع عمل تالك ل 1500 عضو في المجموعات ... وعمل 300 في القنوات .\n قائمه الاوامر /help ",
-                    buttons=(
-                      [
-                         Button.url('𝑫𝒆𝒗', 'https://t.me/W_Q_Z'), 
-                         Button.url('قناة الدعم 💕🍂', 'https://t.me/K_p_s_6'), 
-                      ], 
-                      [
-                        Button.url('ضفني الي مجموعتك 🙊💕', 'https://t.me/Q_X2BOT?startgroup=true'),   
-                      ]
-                   ), 
-                    link_preview=False
-                   )
 
 #help
-@xavierbot.on(events.NewMessage(pattern="^/help$"))
+@xavierbot.on(events.NewMessage(pattern="^/all$"))
 async def help(event):
-  helptext = "**قائمه مساعده بوت التاك**\n\nالأمر: /all \n يمكنك استخدام هذا الأمر مع النص الذي تريد إخبار الآخرين به. \n مثال: `/all هيي ` \n يمكنك استخدام هذا الأمر كإجابة. أي رسالة سيقوم البوت بوضع علامة على المستخدمين للرسالة التي تم الرد عليه"
+  helptext = "**قائمه مساعده بوت التاك**\n\nالأمر: @all \n يمكنك استخدام هذا الأمر مع النص الذي تريد إخبار الآخرين به. \n مثال: `@all هيي ` \n يمكنك استخدام هذا الأمر كإجابة. أي رسالة سيقوم البوت بوضع علامة على المستخدمين للرسالة التي تم الرد عليه"
   await event.reply(helptext,
                     buttons=(
-                      [
-                         Button.url('𝑫𝒆𝒗', 'https://t.me/W_Q_Z'), 
-                         Button.url('قناة الدعم 💕🍂', 'https://t.me/K_p_s_6'), 
-                      ], 
                       [
                         Button.url('ضفني الي مجموعتك 🙊💕', 'https://t.me/TAAG_X2BOT?startgroup=true'),   
                       ]
@@ -76,7 +57,7 @@ async def mentionall(event):
     if msg == None:
         return await event.respond("لا يمكنني ذكر الأعضاء في المنشور القديم !!")
   elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.respond("أعطني شيئاً. مثال: `/all هيي`")
+    return await event.respond("أعطني شيئاً. مثال: `@all هيي`")
   else:
     return await event.respond("قم بالرد علي رساله او اعطني بعض الكلمات لتاك 🤓💕")
     
@@ -132,5 +113,5 @@ async def cancel_mentionall(event):
 
 
 print("تم تنصيب بوت التاك بنجاح 💕🍂")
-print("لو محتاج مساعده @K_P_S_6")
+print("لو محتاج مساعده @MOA_YAD")
 xavierbot.run_until_disconnected()
